9f932aae52d5beaab4895f00c373bca0dcb3455c
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2016-01-04 11:44:05 +1100

    Reduce number of methods on Stack

--- src/lib.rs
+++ src/lib.rs
@@ -156,41 +156,6 @@ impl Stack {
             None => Err(EvalError::StackUnderflow),
         }
     }
-
-    pub fn quote(&mut self) -> EvalResult<()> {
-        let term = try!(self.pop());
-        self.push(Term::Quote(Stack::new(vec![term])));
-        Ok(())
-    }
-
-    fn compose(&mut self, other: Stack) {
-        self.terms.extend(other.terms);
-    }
-
-    fn dup(&mut self) -> EvalResult<()> {
-        let term = try!(self.peek()).clone();
-        self.push(term);
-        Ok(())
-    }
-
-    fn swap(&mut self) -> EvalResult<()> {
-        let term_a = try!(self.pop());
-        let term_b = try!(self.pop());
-
-        self.push(term_a);
-        self.push(term_b);
-
-        Ok(())
-    }
-
-    fn apply_binop<F: Fn(i32, i32) -> i32>(&mut self, f: F) -> EvalResult<()> {
-        let x = try!(self.pop_number());
-        let y = try!(self.pop_number());
-
-        self.push(Term::Number(f(x, y)));
-
-        Ok(())
-    }
 }
 
 impl FromStr for Stack {
@@ -309,7 +274,9 @@ mod prim {
 
     // dup : (A b -> A b b)
     pub fn dup(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.dup()
+        let term = try!(ev.stack.peek()).clone();
+        ev.stack.push(term);
+        Ok(())
     }
 
     // pop : (A b -> A)
@@ -319,7 +286,13 @@ mod prim {
 
     // swap : (A b c -> A c b)
     pub fn swap(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.swap()
+        let term_a = try!(ev.stack.pop());
+        let term_b = try!(ev.stack.pop());
+
+        ev.stack.push(term_a);
+        ev.stack.push(term_b);
+
+        Ok(())
     }
 
     // apply : (A (A -> B) -> B)
@@ -344,7 +317,7 @@ mod prim {
         let stack_a = try!(ev.stack.pop_quote());
         let mut stack_b = try!(ev.stack.pop_quote());
 
-        stack_b.compose(stack_a);
+        stack_b.terms.extend(stack_a.terms);
         ev.stack.push(Term::Quote(stack_b));
 
         Ok(())
@@ -359,28 +332,37 @@ mod prim {
         Ok(())
     }
 
+    fn apply_binop<F: Fn(i32, i32) -> i32>(stack: &mut Stack, f: F) -> EvalResult<()> {
+        let x = try!(stack.pop_number());
+        let y = try!(stack.pop_number());
+
+        stack.push(Term::Number(f(x, y)));
+
+        Ok(())
+    }
+
     // add : (A num num -> A num)
     pub fn add(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.apply_binop(i32::add)
+        apply_binop(&mut ev.stack, i32::add)
     }
 
     // sub : (A num num -> A num)
     pub fn sub(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.apply_binop(i32::sub)
+        apply_binop(&mut ev.stack, i32::sub)
     }
 
     // mul : (A num num -> A num)
     pub fn mul(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.apply_binop(i32::mul)
+        apply_binop(&mut ev.stack, i32::mul)
     }
 
     // div : (A num num -> A num)
     pub fn div(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.apply_binop(i32::div)
+        apply_binop(&mut ev.stack, i32::div)
     }
 
     // rem : (A num num -> A num)
     pub fn rem(ev: &mut Evaluator) -> EvalResult<()> {
-        ev.stack.apply_binop(i32::rem)
+        apply_binop(&mut ev.stack, i32::rem)
     }
 }
